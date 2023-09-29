1c129497d87cd60f8bbd23e43bca3ab9a34426c5
Crhistian Caraballo <cristianjcj35@gmail.com>
2023-09-29 12:05:22 -0500

    fix(theme): fixed theme config

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,2 @@
 import './index.css';
 export * from './lib/atoms';
-export * from '../tailwind.config';
--- packages/core/src/lib/types/theme.ts
+++ packages/core/src/lib/types/theme.ts
@@ -28,3 +28,47 @@ export type PolymorphicComponentPropWithRef<
 // This is the type for the "ref" only
 type PolymorphicRef<C extends React.ElementType> =
   React.ComponentPropsWithRef<C>['ref'];
+
+export const palette = {
+  colors: {
+    gray: {
+      30: '#7D8283',
+      50: '#F9FAFB',
+      300: '#D1D5DB',
+      400: '#9CA3AF',
+      500: '#6B7280',
+      700: '#374151',
+      900: '#111928',
+    },
+    primary: {
+      100: '#59E0E2',
+      200: '#50CACB',
+      300: '#47B3B5',
+      400: '#3E9D9E',
+      500: '#358688',
+      600: '#245A5A',
+      700: '#122D2D',
+    },
+    secondary: {
+      100: '#262930',
+      200: '#43474C',
+      300: '#606567',
+      400: '#7D8283',
+      500: '#358688',
+      600: '#B6BAB9',
+      700: '#D3D6D5',
+    },
+    success: {
+      50: '#F3FAF7',
+      500: '#0E9F6E',
+      600: '#057A55',
+      700: '#046C4E',
+    },
+    error: {
+      50: '#FDF2F2',
+      500: '#F05252',
+      600: '#E02424',
+      700: '#C81E1E',
+    },
+  },
+};
