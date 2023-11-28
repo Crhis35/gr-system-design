6471d4ae6594f7be28efb071c692ad29968b6971
Crhistian Caraballo <cristianjcj35@gmail.com>
2023-11-28 11:41:57 -0500

    fixed merged 5

--- packages/core/src/lib/atoms/indicators/indicator.stories.tsx
+++ packages/core/src/lib/atoms/indicators/indicator.stories.tsx
@@ -11,53 +11,58 @@ export default {
 export const DefaultIndicator = () => (
   // Color circle with a number (Notifications)
   // The TYPE value it receives should be a number
-  <Indicator type={'default'} color={'red'} text={'8'} />
+  <Indicator type={'default'} color={'red'} text={'8'} colorTone={500} />
 );
 
 export const IconIndicator = () => (
   // Circle with a check Icon
-  <Indicator type={'icon'} color={'teal'} text={''} />
+  <Indicator type={'icon'} color={'teal'} text={''} colorTone={600} />
 );
 
 // ---- Legend Indicators ----
 export const LegendIndicator = () => (
   // Circle of color with text
-  <Indicator type={'legend'} color={'#e9e9e9'} text={''} />
+  <Indicator type={'legend'} color={'gray'} text={''} colorTone={200} />
 );
 
 export const PrimaryLegendIndicator = () => (
   // Circle of color with text
-  <Indicator type={'legend'} color={'#3E9D9E'} text={''} />
+  <Indicator type={'legend'} color={'primary'} text={''} colorTone={600} />
 );
 
 export const PurpleLegendIndicator = () => (
   // Circle of color with text
-  <Indicator type={'legend'} color={'#9061F9'} />
+  <Indicator type={'legend'} color={'red'} text={''} colorTone={500} />
 );
 
 export const IndigoLegendIndicator = () => (
   // Circle of color with text
-  <Indicator type={'legend'} color={'indigo'} text={''} />
+  <Indicator type={'legend'} color={'indigo'} text={''} colorTone={500} />
 );
 
 export const TealLegendIndicator = () => (
   // Circle of color with text
-  <Indicator type={'legend'} color={'teal'} text={''} />
+  <Indicator type={'legend'} color={'teal'} text={''} colorTone={500} />
 );
 
 // --- || ---
 // ---- Badge Indicator ----
 export const BadgeIndicator = () => (
   // Same as legend but with Background color depending on the circle's state Available/Unavailable
-  <Indicator type={'badgeAvailable'} color={'teal'} text={''} />
+  <Indicator type={'badgeAvailable'} color={'teal'} text={''} colorTone={600} />
 );
 
 export const RedBadgeIndicator = () => (
   // Same as legend but with Background color depending on the circle's state Available/Unavailable
-  <Indicator type={'badgeUnavailable'} color={'red'} text={''} />
+  <Indicator
+    type={'badgeUnavailable'}
+    color={'red'}
+    text={''}
+    colorTone={600}
+  />
 );
 
 export const StepperIndicator = () => (
   // Checkbox for steps (readonly)
-  <Indicator type={'stepper'} color={'teal'} text={''} />
+  <Indicator type={'stepper'} color={'teal'} text={''} colorTone={600} />
 );
